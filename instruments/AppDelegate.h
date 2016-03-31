52885598b8a1d74e2387d2810ecb4ecbfc1ae0b3
jiedaibao <jiedaibao@jiedaibaodemacbook-pro.local>
2016-03-31 19:29:32 +0800

    Initial Commit

---
+++ instruments/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instruments
+//
+//  Created by jiedaibao on 16/3/31.
+//  Copyright © 2016年 jiedaibao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
